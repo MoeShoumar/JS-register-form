4ac52a8dec40d5f976f6deb4c652a90690f731cc
MoeShoumar <mks38@mail.aub.edu>
2023-02-25 17:45:08 +0200

    selected id's in js file and added validation function

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JS-register-form
 Language Used: HTML,CSS and JS.
-About: This is a Register Form with 5 input fields such as password, age and strings.
+
+About: This is a Registeration Form with 5 input fields such as password, age and strings. It's main objective is to practice DOM manipulation in JS.
--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-function goBack() {
-    window.location.href = 'index.html'
-}
+// Button that revert to page 1
+let goBack = () => window.location.href = 'index.html'
+// Form validation
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const form = document.querySelector('#form');
+const error = document.querySelector('#error');
+const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+// Email validation
+form.addEventListener('submit', (e) => {
+    let massages = []
+    if (email.value === '' || email.value === null || email.value === '') {
+        massages.push('wrong email or password format!')
+    }
+    if (password.value === '' || password.value === null
+        || password.value.length < 8) {
+        massages.push('wrong email or password format!')
+    }
+    if (massages.length > 0) {
+        e.preventDefault()
+        error.innerHTML = massages.join(', ')
+    }
+})
+// let validateForm = () =>
+
+//     function validateForm() {
+//         if (email.innerHTML
+//     }
+
+// Password should contain 8 characters minimum, one special character minimum,
+//  at least one upper case letter,  etc.
+// An email should have also the email format: @, no dashes, a "dot" somewhere after the @.
